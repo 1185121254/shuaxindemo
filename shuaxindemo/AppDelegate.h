14647bd2a3d8337dc2f4c03d30166ae34258525b
chaojie <chaojie@chaojiedemac-mini.local>
2017-07-04 16:27:24 +0800

    Initial Commit

---
+++ shuaxindemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shuaxindemo
+//
+//  Created by chaojie on 2017/7/4.
+//  Copyright © 2017年 chaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shuaxindemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shuaxindemo
+//
+//  Created by chaojie on 2017/7/4.
+//  Copyright © 2017年 chaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
